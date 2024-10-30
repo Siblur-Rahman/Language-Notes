3586524317d794319171dcfa285620d113b2c8a2
MS Rahman <msrahman048gmail.com>
2024-10-31 00:42:11 +0600

    31/10/24 from pc

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes'
-import AuthProvider from './providers/AuthProvider'
 import { Toaster } from 'react-hot-toast'
 import { ToastContainer } from 'react-toastify'
 import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      {/* <RouterProvider router={router} /> */}
       <HelmetProvider>
             <div className='max-w-screen-xl mx-auto'>
               <RouterProvider router={router} />
             </div>
       </HelmetProvider>
       <Toaster />
-    </AuthProvider>
     <ToastContainer/>
   </React.StrictMode>
 )
